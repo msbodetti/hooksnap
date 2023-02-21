60795d504012d5d904f183372294c725ce534664
Yentl Naidoo <yentl.naidoo@mtn.com>
2023-02-21 16:52:07 +0200

    Vue project with tailwind, typescript, lint and cypress support

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+     This is the home view
+  </h1>
+</template>
